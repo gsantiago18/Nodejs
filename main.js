b0331a06c791a7a0cb5301c2a265393b86994932
Gabodev <gsantiago.glwyton@gmail.com>
2025-03-24 21:13:22 -0500

    Creando crud sencillo con express y node.js

---
+++ main.js
@@ -0,0 +1,35 @@
+const {Client} = require ('pg');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+const con = new Client({
+    hsot : 'localhost',
+    user : 'postgres',
+    port : 5432,
+    password : '',
+    database: 'salidasdemo'
+})
+
+con.connect().then(()=> console.log ('connected'));
+
+app.post('/postData',  (req, res) =>{
+ 
+ const {codsal, description, fecha } = req.body;
+ 
+ const insert_query = 'INSERT INTO salida (codsal,description, fecha) VALUES ($1 ,$2, $3)'
+ con.query(insert_query,[codsal,description,fecha], (err, result)=>{
+    if(err){
+        res.status(400).send(err);
+    }else{
+        res.status(200).send('Data inserted successfully');
+    }
+ })
+
+})
+
+app.listen (4000,()=>{
+    console.log('Server is running on port 4000');
+})
